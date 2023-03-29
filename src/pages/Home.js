4ece45f6d76b4dba7566b56dd0f5c8cabbaa5faa
Ismail Hosny <76536233+Izmil-H@users.noreply.github.com>
2023-03-29 11:36:38 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function HomePage() {
       <h1>Ismail Hosny</h1>
       <NavBar />
       <p class={styles.aboutMe}>
-        I am an 11th grade student at Earl Haig Secondary School
+        I am a 12th grade student at Earl Haig Secondary School
       </p>
     </section> 
 )}
